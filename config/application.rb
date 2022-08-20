4be8e57fe4a2871c53d439dd90991c754309b630
kona <kitami.kitano20@gmail.com>
2022-08-20 10:28:30 +0900

    :globe_with_meridians: i18n対応

--- config/application.rb
+++ config/application.rb
@@ -23,13 +23,7 @@ module AwesomeEvents
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
     config.time_zone = "Tokyo"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :ja
   end
 end
