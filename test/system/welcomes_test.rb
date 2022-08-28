b4fafa9bb5fe1d65093d9a3a8c8bb0976aa181e8
kona <kitami.kitano20@gmail.com>
2022-08-28 10:40:20 +0900

    :memo: minitestをrspecに移行

--- test/system/welcomes_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class WelcomesTest < ApplicationSystemTestCase
-  test "/ ページを表示" do
-    visit root_url
-
-    assert_selector "h1", text: "イベント一覧"
-  end
-end
