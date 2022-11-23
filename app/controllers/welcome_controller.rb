48476f4ca9b19978521d48f8abdd279d4bbcc95c
kona <kitami.kitano20@gmail.com>
2022-11-23 18:14:57 +0900

    :memo: [Add]rootページにログインユーザーを記載

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,5 +4,6 @@ class WelcomeController < ApplicationController
   def index
     # @events = Event.page(params[:page]).per(10).where("start_at > ?", Time.zone.now).order(:start_at)
     @events = Event.page(params[:page]).per(10).order(:start_at) # 過ぎた日程のイベントでも表示する
+    @current_user = current_user
   end
 end
